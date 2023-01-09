f30d4eb68003bbe53b3df4519d481d5a29f6d184
João H. A. Ribeiro <113396206+3ovospor1real@users.noreply.github.com>
2023-01-09 12:12:25 -0300

    hora e data adicionados

--- clock/scripts.js
+++ clock/scripts.js
@@ -1,11 +1,10 @@
 
 const body = document.getElementsByTagName('body')[0];
 
+
 // obtem as informações de hora, dia, mês e ano
 let now = new Date()
-let array = {diaSemana: weekDay(), mes: now.getMonth(), hora: now.getHours()}
-
-array.hora = 18
+let array = {ano: now.getFullYear(), diaSemana: weekDay(), mes: month(),diaMes:now.getDate(), hora: now.getHours(), minuto: String(now.getMinutes())}
 
 // altera a imagem de fundo da página
 if( array.hora >= 6 && array.hora < 12) {
@@ -19,6 +18,30 @@ if( array.hora >= 6 && array.hora < 12) {
     //noite
     body.style.backgroundImage = 'url(image/noite.jpg)'
 }
+
+// dia da semana
+const diaSemana = document.querySelector('h1#dia_semana');
+diaSemana.innerHTML = array.diaSemana
+
+// horas e minutos
+const hora = document.querySelector('span#hora');
+if(String(array.hora).length == 1) array.hora = '0' + array.hora
+hora.innerHTML = array.hora
+const minuto = document.querySelector('span#minuto');
+if(array.minuto.length == 1) array.minuto = '0'+array.minuto
+minuto.innerHTML = array.minuto
+
+// dia do mês
+const diaMes = document.querySelector('span#dia_mes');
+diaMes.innerHTML = array.diaMes
+
+// mês
+const mes = document.querySelector('span#mes');
+mes.innerHTML = array.mes
+
+//ano
+const ano = document.querySelector('span#ano');
+ano.innerHTML = array.ano
 
 //função para definir o dia da semana
 function weekDay(){
@@ -44,5 +67,46 @@ function weekDay(){
         case 6:
             return 'Sábado'
             break
+    }
+}
+
+function month() {
+    switch (now.getMonth()) {
+        case 0:
+            return 'Janeiro'
+            break;
+        case 1:
+            return 'Fevereiro'
+            break;
+        case 2:
+            return 'Março'
+            break;
+        case 3:
+            return 'Abril'
+            break;
+        case 4:
+            return 'Maio'
+            break;
+        case 5:
+            return 'Junho'
+            break;
+        case 6:
+            return 'Julho'
+            break;
+        case 7:
+            return 'Agosto'
+            break;
+        case 8:
+            return 'Setembro'
+            break;
+        case 9:
+            return 'Outubro'
+            break;
+        case 10:
+            return 'Novembro'
+            break;
+        case 11:
+            return 'Dezembro'
+            break;
     }
 }
