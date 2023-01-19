5cea8052a438f2374b29205f3c831d40a636eef1
João H. A. Ribeiro <113396206+3ovospor1real@users.noreply.github.com>
2023-01-18 22:36:21 -0300

    calculador de imc iniciado

---
+++ calculador-imc/scripts.js
@@ -0,0 +1,11 @@
+/* 
+    IMC = peso/ altura(em metros) ^ 2
+
+    Menor que 17  = Muito abaixo do peso
+    17 a 18.5     = Abaixo do peso
+    18.5 a 24.9   = Peso normal
+    25 a 29.9     = Acima do Peso
+    30 a 34.9     = Obesidade
+    35 a 39.9     = Obesidade (severa)
+    Acima de 40   = Obesidade (mórbita)
+    */
