2bc11fbe1d75cd0eb40632d4e7a2420e6b3b8ba0
João H. A. Ribeiro <113396206+3ovospor1real@users.noreply.github.com>
2023-02-20 16:18:27 -0300

    nav bar funcionando

---
+++ emti/assets/js/menu.js
@@ -0,0 +1,11 @@
+
+const menuBtn = document.querySelector('#menu');
+const nav = document.querySelector('.menu-escondido');
+
+menuBtn.addEventListener('click', (e) => {
+    if(nav.style.display === '') {
+        nav.style.display = 'block'
+    } else {
+        nav.style.display = ''
+    }
+})
